e32a1809e82a3202a75455b1809914ff99e9a79a
java-w3schools <venkivenki4b6@gmail.com>
2020-05-06 20:08:23 +0530

    first commit

---
+++ src/main/java/com/javaprogramto/springboot/SpringBootCofigurations/SpringBootCofigurationsApplication.java
@@ -0,0 +1,13 @@
+package com.javaprogramto.springboot.SpringBootCofigurations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCofigurationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCofigurationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaprogramto/springboot/SpringBootCofigurations/SpringBootCofigurationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaprogramto.springboot.SpringBootCofigurations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCofigurationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
