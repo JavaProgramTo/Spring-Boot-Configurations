c39e52d5cc7b13cae633fc868d1ae63870819cbc
java-w3schools <venkivenki4b6@gmail.com>
2020-05-06 21:59:34 +0530

    How to Change the Default Port in Spring Boot

---
+++ src/main/java/com/javaprogramto/springboot/SpringBootCofigurations/CustomEmbededPortChange.java
@@ -0,0 +1,14 @@
+package com.javaprogramto.springboot.SpringBootCofigurations;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomEmbededPortChange implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.setPort(8086);
+    }
+}
--- src/main/java/com/javaprogramto/springboot/SpringBootCofigurations/SpringBootCofigurationsApplication.java
+++ src/main/java/com/javaprogramto/springboot/SpringBootCofigurations/SpringBootCofigurationsApplication.java
@@ -3,11 +3,27 @@ package com.javaprogramto.springboot.SpringBootCofigurations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class SpringBootCofigurationsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCofigurationsApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+
+       // SpringApplication.run(SpringBootCofigurationsApplication.class, args);
+
+        SpringApplication app = new SpringApplication(SpringBootCofigurationsApplication.class);
+
+        Map<String, Object> customConfig = new HashMap<>();
+        customConfig.put("server.port", "9009");
+
+       // app.setDefaultProperties(customConfig);
+
+        app.run(args);
+
+    }
+
 
 }
